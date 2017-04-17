af072fea967b2b0d6985886720db83ef7244bbc7
darci <darcikhey27@gmail.com>
2017-04-17 00:09:39 -0700

    added first files

---
+++ lynda_file/Exercise Files/Chap05/AccessingDOM/script.js
@@ -0,0 +1,9 @@
+
+
+var mainTitle = document.getElementById("mainTitle");
+
+console.log("this is a element of tyepe, ", mainTitle.nodeType);
+
+console.log("the inner html is ", mainTitle.innerHTML);
+
+console.log("Child nodes: ", mainTitle.childNodes);
