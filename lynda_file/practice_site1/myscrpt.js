ab07baa643d247a09e9b51c5a6f889d2cededf5b
Darci Khey Saucedo <darcikhey27@gmail.com>
2017-04-19 10:46:09 -0700

    javascript

--- lynda_file/practice_site1/myscrpt.js
+++ lynda_file/practice_site1/myscrpt.js
@@ -2,4 +2,10 @@
  * Created by DARCI on 4/18/2017.
  */
 
-var page = document.getElementsByName("body")
+var item = document.getAnonymousElementByAttribute("div");
+
+var num_items = item.length;
+
+item.onclick = function () {
+    alert("num items were ", num_items)
+}
